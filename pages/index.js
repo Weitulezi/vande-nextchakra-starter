00d035e814ba45a292ca73b5488c264dac92195a
Dixon Vande <vandenemo@yahoo.com>
2022-02-27 16:36:12 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Home() {
         flexDirection='column'
         rowGap='20px'
       >
-        <Text as='h1' fontSize="30" align='center' fontWeight='bold'>
+        <Text as='h1' fontSize={['30px', '40px', '40px']} textAlign='center' fontWeight='bold'>
           Vande-NextJS-Chakra-UI
         </Text>
         <Code width='fit-content' align='center' mx='auto' p='5px 10px' borderRadius='5px'>
           npx create-next-app --example https://github.com/weitulezi/vande-nextchakra-starter [YOUR_APP_NAME]
         </Code>
-        <Text as='h4'>Pre installed dependencies</Text>
+        <Text as='h4'>Pre installed dependencies : </Text>
         <HStack spacing={3} color='white' wrap='wrap' rowGap={3} justifyContent='center' alignItems='center'>
           <Text as='h3' fontSize='18px' bg="customBlue" p="2px 8px" borderRadius={5}>chakra-ui</Text>
           <Text as='h3' fontSize='18px' bg="customRed" p="2px 8px" borderRadius={5}>react-icons</Text>
